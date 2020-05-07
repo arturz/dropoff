59d673baf5a9a3883c87939c310edb407c7aefdf
Artur <26014458+xout@users.noreply.github.com>
2020-05-07 22:06:38 +0200

    Remove dist directory

--- dist/lib/utils/isUrl.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const url_regex_1 = __importDefault(require("url-regex"));
-exports.default = (path) => url_regex_1.default().test(path);
